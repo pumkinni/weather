d4390c02b24a366a4503d5f59b0f797ec33246b2
pumkinni <cindy4766@naver.com>
2023-03-18 16:44:25 +0900

    add weather diary CRUD

--- src/main/java/zerobase/weather/controller/DiaryController.java
+++ src/main/java/zerobase/weather/controller/DiaryController.java
@@ -1,13 +1,12 @@
 package zerobase.weather.controller;
 
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import zerobase.weather.domain.Diary;
 import zerobase.weather.service.DiaryService;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 public class DiaryController {
@@ -17,10 +16,37 @@ public class DiaryController {
         this.diaryService = diaryService;
     }
 
+    // 다이어리 생성
     @PostMapping("/create/diary")
     void createDiary(@RequestParam
                      @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date, // date 타입이 다양해 포맷을 정하기 위해서 사용
                      @RequestBody String text) {
         diaryService.createDiary(date, text);
+    }
+
+    // 특정 날짜의 다이어리 값들을 가져오기
+    @GetMapping("/read/diary")
+    List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date){
+        return diaryService.readDiary(date);
+    }
+
+    // 특정 기간동안의 다이어리 값들을 가져오기
+    @GetMapping("/read/diaries")
+    List<Diary> readDairies(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+                            @RequestParam@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate){
+        return diaryService.readDiaries(startDate, endDate);
+    }
+
+    // 다이어리 수정
+    @PutMapping("/update/diary")
+    void upgradeDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+                      @RequestBody String text){
+        diaryService.updateDiary(date, text);
+    }
+
+    // 다이어리 삭제
+    @DeleteMapping("/delete/diary")
+    void deleteDairy(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date){
+        diaryService.deleteDiary(date);
     }
 }
